467a44ea5a33d97e1df9aab7ca312cb9967f25dc
yeqianfeng911@gmail.com <yeqianfeng@NicoledeiMac.local>
2019-12-16 00:42:06 +0800

    add wx mini program
    Signed-off-by: yeqianfeng911@gmail.com <yeqianfeng@NicoledeiMac.local>

---
+++ lyxmini/pages/login/login.js
@@ -0,0 +1,97 @@
+// pages/login/login.js
+//获取应用实例
+const app = getApp();
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    username: "",
+    password: ""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {},
+
+  usernameInput: function(e) {
+    this.setData({
+      username: e.detail.value
+    });
+  },
+
+  // 获取输入密码
+  passwordInput: function(e) {
+    this.setData({
+      password: e.detail.value
+    });
+  },
+
+  // 登录
+  login: function() {
+    if (this.data.username.length == 0 || this.data.password.length == 0) {
+      wx.showToast({
+        title: "用户名和密码不能为空",
+        icon: "loading",
+        duration: 1000
+      });
+    } else {
+      // 这里修改成跳转的页面
+      wx.request({
+        url: app.globalData.baseUrl + "/login",
+        method: "POST",
+        data: {
+          username: this.data.username,
+          password: this.data.password
+        },
+        success(res) {
+          console.log(res.data);
+
+          wx.setStorageSync("Access-Token", res.data.access_token);
+        }
+      });
+      wx.showToast({
+        title: "登录成功",
+        icon: "success",
+        duration: 2000
+      });
+    }
+  }
+});
